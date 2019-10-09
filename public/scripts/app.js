e3a6bfc959e4530f58565ca65ef46181d1d7d4b9
Darrenni97 <Darrenni_@hotmail.com>
2019-10-08 20:04:03 -0700

    implemented dynamic tweets with moments js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,41 +1,64 @@
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+// loops through tweets
+  for (tweet in tweets) {
+    // calls createTweetElement for each tweet
+      $('#tweets').append(createTweetElement(tweets[tweet]));
+  }
+  // takes return value and appends it to the tweets container
 }
 
 const createTweetElement = function(tweet) {
-const newElement = `
-  <article class='tweet-container'>
-    <header class='tweet-header'>
-      <div id='tweet-profile'>
-        <img id='profile-img' src='${tweet.user.avatars}'>
-        <span>${tweet.user.name}</span>
-      </div>
-      <span id='tweet-username'>${tweet.user.handle}</span>
-    </header>
-    <span id='tweet-text'>${tweet.content.text}</span>
-      <footer>
-      <span id='timestamp'>${tweet.created_at}</span> 
-      <span id='tweet-icons'>
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </span>
-    </footer>
-  </article>
-`
-return newElement;
+  const time = moment(tweet['created_at']).fromNow();
+  const newElement = `
+    <article class='tweet-container'>
+      <header class='tweet-header'>
+        <div id='tweet-profile'>
+          <img id='profile-img' src='${tweet['user']['avatars']}'>
+          <span>${tweet['user']['name']}</span>
+        </div>
+        <span id='tweet-username'>${tweet['user']['handle']}</span>
+      </header>
+      <span id='tweet-text'>${tweet['content']['text']}</span>
+        <footer>
+        <span id='timestamp'>${time}</span> 
+        <span id='tweet-icons'>
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </span>
+      </footer>
+    </article>
+  `
+  return newElement;
 }
 
-const $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet);
+$(document).ready(function() {
+  renderTweets(data);
+})
